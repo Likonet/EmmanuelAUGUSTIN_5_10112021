32eaf2189393de01063a88058586e1fa49f58817
Likonet <dyplomats@hotmail.com>
2021-11-23 21:40:50 -0300

    Divers champs produits

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -15,12 +15,19 @@ fetch("http://localhost:3000/api/products/"+idSofa)
             document.getElementById('title').textContent= products.name;
             document.getElementById('price').textContent= products.price;
             document.getElementById('description').textContent= products.description;
+            let hello =  document.querySelector('.item__img');
+            console.log(hello);
             document.querySelector('.item__img').innerHTML= `<img src="${products.imageUrl}" alt="${products.altTxt}"></img>`
-         
-
-           
-      
-
+            console.log(products.colors[1]);
+            let toto =  document.getElementById('colors');
+            console.log(toto);
+            document.getElementById('colors').innerHTML=`
+            <option value="">--SVP, choisissez une couleur --</option>
+            <option value="blue">${products.colors[1]}</option>
+            <option value="${products[1].colors}">${products[1].colors}</option>
+            <option value="${products[2].colors}">${products[2].colors}</option>
+            <option value="${products[3].colors}">${products[3].colors}</option>
+            `
     })
     .catch(function(err) {
       
