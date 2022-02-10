40cc19b5468d534f7196a00e67875b65aa50c7aa
Likonet <dyplomats@hotmail.com>
2022-02-09 21:20:09 -0300

    fonction ajout en local storage
    ajout en localstorage

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -1,7 +1,7 @@
 var url = new URL(document.location);
-console.log(url);
+//console.log(url);
 const idSofa = url.searchParams.get("id");
-console.log(idSofa);
+//console.log(idSofa);
 fetch("http://localhost:3000/api/products/"+idSofa)
     .then(function(res) {
       if (res.ok) {
@@ -9,27 +9,71 @@ fetch("http://localhost:3000/api/products/"+idSofa)
       }
     })
     .then(function(value) {
-    console.log(value);
+    //console.log(value);
 
         let products = value;    
             document.getElementById('title').textContent= products.name;
             document.getElementById('price').textContent= products.price;
             document.getElementById('description').textContent= products.description;
-            let hello =  document.querySelector('.item__img');
-            console.log(hello);
+           // console.log(hello);
             document.querySelector('.item__img').innerHTML= `<img src="${products.imageUrl}" alt="${products.altTxt}"></img>`
-            console.log(products.colors[1]);
-            let toto =  document.getElementById('colors');
-            console.log(toto);
-            document.getElementById('colors').innerHTML=`
-            <option value="">--SVP, choisissez une couleur --</option>
-            <option value="blue">${products.colors[1]}</option>
-            <option value="${products[1].colors}">${products[1].colors}</option>
-            <option value="${products[2].colors}">${products[2].colors}</option>
-            <option value="${products[3].colors}">${products[3].colors}</option>
-            `
+           // console.log(products.colors[1]);
+           // console.log(toto);
+            document.getElementById('colors').innerHTML=`  
+            <option value="">--SVP, choisissez une couleur --</option>`
+            for(let i = 0 ; i< products.colors.length ; i++){
+              document.getElementById('colors').innerHTML +=`<option value="${products.colors[i]}">${products.colors[i]}</option>`
+            }
+                
     })
-    .catch(function(err) {
+    .catch(function() {
       
     });
 
+    document.getElementById('addToCart').addEventListener('click', (e) => {
+      const productToOrder= getProductFromForm(idSofa);
+      addProductToLocalStorage(productToOrder);
+      
+    })
+    
+
+    function getProductFromForm(idSofa){
+    
+      const color = document.getElementById('colors').value;
+      const quantity= document.getElementById('quantity').value;
+      const productToOrder = { 
+        id: idSofa,
+        color,
+        quantity   
+      };
+      return productToOrder;
+    
+     
+    }
+
+    function addProductToLocalStorage(productToOrder)
+    
+    {
+  //condition ternaire : "Est-ce que " ? "oui" : "non"
+  let panier = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+  console.log(panier)
+  // je dois regarder : 
+  // Est ce que le produit que j'ajoute il a le meme id et la meme couleur que un produit qui est deja dans le panier ?
+  // On cherche dans le panier
+  let findProduct = panier.find(elt => elt.id === productToOrder.id && elt.color === productToOrder.color)
+  console.log(findProduct)
+  console.log(productToOrder)
+  
+  if(findProduct){
+    findProduct.quantity = parseInt(findProduct.quantity) + parseInt(productToOrder.quantity); 
+    console.log(findProduct.quantity)
+  }else{
+    panier.push(productToOrder);
+  }
+  localStorage.setItem('cart', JSON.stringify(panier));
+  
+}
+
+   
+
+   
