6c232665db958baa9118922f3e90011872384d86
Likonet <dyplomats@hotmail.com>
2022-02-21 22:18:42 -0300

    FORMULAIRE
    FORMULAIRE

--- P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -8,10 +8,24 @@ fetch("http://localhost:3000/api/products")
     
          let toto = localStorage.getItem('cart');   
          let list = JSON.parse(toto);
-         console.log(toto)
-         console.log(list)
+         console.log(toto);
+         console.log(list);
+       
+         console.log (list.length)
+         console.log (list[3].quantity)
+          let totalQuantity = 0
+          let productID = []
+      
+          let sum = list.values();
+          for (const tees of sum) {
+            totalQuantity += parseInt(tees.quantity);
+            productID.push(tees.id);
+            
+          }
+         console.log (productID)
+         
          for (let hello of list) {
-            console.log (hello.id)
+             console.log(hello)
             document.getElementById('cart__items').innerHTML +=
             `<article class="cart__item" data-id="${hello.id}">
             <div class="cart__item__img">
@@ -20,8 +34,12 @@ fetch("http://localhost:3000/api/products")
             <div class="cart__item__content">
               <div class="cart__item__content__titlePrice">
                 <h2>${hello.nameSofa}</h2>
-                <p>${hello.price}</p>
+                <p class= toto>${hello.price*hello.quantity}</p>
               </div>
+              <div class="cart__item__content__color">
+                <h3 id="color_sofa">${hello.color}</h2>
+              </div>
+
               <div class="cart__item__content__settings">
                 <div class="cart__item__content__settings__quantity">
                   <p>Qté: </p>
@@ -33,56 +51,88 @@ fetch("http://localhost:3000/api/products")
               </div>
             </div>
           </article> `;
-          
-          Array.from(document.querySelectorAll('.itemQuantity')).forEach(item => { // for each chaque élément du tableau
+         
+        let totalPrice = 0
+         Array.from(document.querySelectorAll('p.toto')).forEach(roro);
+         function roro(item) {
+          totalPrice += parseInt(item.innerHTML);
+          console.log(totalPrice)
+                             }
+        console.log(totalPrice)
+          document.getElementById("totalPrice").innerText = totalPrice; 
+        
+        
+
+        document.getElementById("totalQuantity").innerText = totalQuantity;
+
+          Array.from(document.querySelectorAll('.itemQuantity')).forEach(item => { // récupération de tous les éléments et les insérer dans un tableau.
           item.addEventListener('change', (e) => {
-            let nameOfSofa = e.target.closest('article').querySelector('h2').innerText ;
+            let nameOfSofa = e.target.closest('article').querySelector('h2').innerText ; // récupération du 
+            let colorSofa = e.target.closest('article').querySelector('#color_sofa').innerText ;
             let newQuantity = e.target.value
             console.log(e.target.value) 
-            console.log(nameOfSofa);
-            //console.log (number);
+            console.log(colorSofa);
+            console.log (list);
+            console.log (nameOfSofa);
+
 
             let tesst = list.find (elt => elt.nameSofa === nameOfSofa);
-            let indiceArray = list.findIndex(elt => elt.nameSofa === nameOfSofa);
-            console.log (tesst)
-            console.log (tesst)
+           let indiceArray = list.findIndex (elt => elt.nameSofa === nameOfSofa && elt.color === colorSofa);
+           console.log (indiceArray);
             list[indiceArray].quantity = newQuantity;
-            console.log(list);
             localStorage.clear();
-            
             localStorage.setItem('cart', JSON.stringify(list));
-
-
+          window.location.reload()
           }) 
-       
+        
            })
 
            Array.from(document.querySelectorAll('.deleteItem')).forEach(item => { // for each chaque élément du tableau
             item.addEventListener('click', (e) => {
-              let nameOfSofa = e.target.closest('article').querySelector('h2').innerText ;
-              let indiceArray = list.findIndex(elt => elt.nameSofa === nameOfSofa);
-                if (indiceArray > -1) {
-             list.splice(indiceArray, 1);
-                }
-              console.log(list)
-              
-              localStorage.setItem('cart', JSON.stringify(list));
-             window.location.reload()
+            let nameOfSofa = e.target.closest('article').querySelector('h2').innerText ;
+            let indiceArray = list.findIndex(elt => elt.nameSofa === nameOfSofa);
+            list.splice(indiceArray, 1);
+            localStorage.setItem('cart', JSON.stringify(list));
+            window.location.reload()
   
             }) 
          
              })
-          
-          
-          // Array.from(document.getElementsByClassName('deleteItem')).forEach(item => {
-          
-           // item.addEventListener('click', (e) => {
-           
-              //  var link = e.target.closest('a');
-          //  })})
+    
 
+             document.getElementById("order").addEventListener("submit", (e) => {send(e)});
+             function send(e) {
+                e.preventDefault();
+                // 1. Je viens récupérer les donnnées du formulaire
+                let contact = {
+                    firstName: document.getElementById("firstName").value,
+                    lastName: document.getElementById("lastName").value,
+                    address: document.getElementById("address").value,
+                    city: document.getElementById("city").value,
+                    email: document.getElementById("email").value
+                  }
+                // 2. Je viens verifier les donnees du formulaire
 
+                // 3. je fais un tableau avec les productsId
+                // a completer => productID
+                // 3. J'envoie au serveur
+                fetch("http://localhost:3000/api/products", {
+                  method: "POST",
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({contact, productID})
 
+                })
+                .then(function(res) {
+                  if (res.ok) {
+                    return res.json();
+                  }
+                })
+                .then(function(value) {
+                    console.log(value)
+                });
+              }
 
         }
         })
